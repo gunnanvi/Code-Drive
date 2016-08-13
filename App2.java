8599ce1d4ea7729848654c3d7eaa7b0c08452897
gunnanvi <nehakaran3033@gmail.com>
2016-08-13 10:15:24 +0530

    Add files via upload

---
+++ App2.java
@@ -0,0 +1,19 @@
+
+public class App2 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1, r = 5; i <= 5; i++, r--) {
+			
+			for (int j = 1 ; j <= r; j++) {
+				
+				System.out.print(j);
+			}
+			System.out.println(" ");
+			
+		}
+		
+
+	}
+
+}
